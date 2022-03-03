d768d160c6d06fdd6d86023956dbc057a33668d0
Pavan <pavankumar@divami.com>
2022-03-03 14:10:57 +0530

    opportunities mock response added

--- node-output/service/OpportunityService.js
+++ node-output/service/OpportunityService.js
@@ -55,7 +55,76 @@ exports.findAllOpportunities = function() {
       "isHelpfulSelected" : false,
       "category" : "Critical Care: Insufficient critical care statement",
       "isNotHelpfulSelected" : false
-    } ]
+    }, ]
+  }, {
+    "reviewRequestedPhysicianName" : "M. Sing",
+    "isPeerReviewRequested" : true,
+    "patientInfo" : {
+      "firstName" : "Andrew",
+      "lastName" : "Lange",
+      "gender" : "M",
+      "dateOfService" : "24-May-2021",
+      "age" : 39
+    },
+    "opportunities" : [ {
+      "isEducationalTipAvailable" : false,
+      "comment" : "Patient appears to be partially responding or appears to have minor complication.",
+      "isHelpfulSelected" : false,
+      "category" : "Critical Care: Insufficient critical care statement",
+      "isNotHelpfulSelected" : false
+    }, {
+      "isEducationalTipAvailable" : false,
+      "comment" : "Patient appears to be partially responding or appears to have minor complication.",
+      "isHelpfulSelected" : false,
+      "category" : "Critical Care: Insufficient critical care statement",
+      "isNotHelpfulSelected" : false
+    }, ]
+  }, {
+    "reviewRequestedPhysicianName" : "M. Sing",
+    "isPeerReviewRequested" : true,
+    "patientInfo" : {
+      "firstName" : "Andrew",
+      "lastName" : "Lange",
+      "gender" : "M",
+      "dateOfService" : "24-May-2021",
+      "age" : 39
+    },
+    "opportunities" : [ {
+      "isEducationalTipAvailable" : false,
+      "comment" : "Patient appears to be partially responding or appears to have minor complication.",
+      "isHelpfulSelected" : false,
+      "category" : "Critical Care: Insufficient critical care statement",
+      "isNotHelpfulSelected" : false
+    }, {
+      "isEducationalTipAvailable" : false,
+      "comment" : "Patient appears to be partially responding or appears to have minor complication.",
+      "isHelpfulSelected" : false,
+      "category" : "Critical Care: Insufficient critical care statement",
+      "isNotHelpfulSelected" : false
+    }, ]
+  }, {
+    "reviewRequestedPhysicianName" : "M. Sing",
+    "isPeerReviewRequested" : true,
+    "patientInfo" : {
+      "firstName" : "Andrew",
+      "lastName" : "Lange",
+      "gender" : "M",
+      "dateOfService" : "24-May-2021",
+      "age" : 39
+    },
+    "opportunities" : [ {
+      "isEducationalTipAvailable" : false,
+      "comment" : "Patient appears to be partially responding or appears to have minor complication.",
+      "isHelpfulSelected" : false,
+      "category" : "Critical Care: Insufficient critical care statement",
+      "isNotHelpfulSelected" : false
+    }, {
+      "isEducationalTipAvailable" : false,
+      "comment" : "Patient appears to be partially responding or appears to have minor complication.",
+      "isHelpfulSelected" : false,
+      "category" : "Critical Care: Insufficient critical care statement",
+      "isNotHelpfulSelected" : false
+    }, ]
   } ]
 };
     if (Object.keys(examples).length > 0) {
