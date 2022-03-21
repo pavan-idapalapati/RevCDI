6033f20bda6428dae9019456a8851f8ad32d95b4
pavan-idapalapati <pavankumar.idapalapati@gmail.com>
2022-03-21 06:53:45 +0000

    API definition transferred by SwaggerHub

---
+++ node-output/service/UserDetailsService.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+/**
+ * Get user details and unread responses
+ *
+ * returns inline_response_200_1
+ **/
+exports.userDetailsGET = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "unReadResponses" : [ {
+    "cdiSpecialist" : {
+      "firstName" : "Puru",
+      "lastName" : "vanteru"
+    },
+    "message" : "Yes, we run all out opportunities through the...",
+    "timestamp" : "10/03/2022 17:35:44"
+  }, {
+    "cdiSpecialist" : {
+      "firstName" : "Puru",
+      "lastName" : "vanteru"
+    },
+    "message" : "Yes, we run all out opportunities through the...",
+    "timestamp" : "10/03/2022 17:35:44"
+  } ],
+  "userDetails" : {
+    "firstName" : "A.",
+    "lastName" : "Osman",
+    "imageUrl" : "/ProfileImage.svg",
+    "specialization" : "MD",
+    "id" : 1
+  }
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
