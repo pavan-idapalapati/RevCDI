9aac1d7879eccde5fe3ee6303a0d3f02feb96a62
pavan-idapalapati <pavankumar.idapalapati@gmail.com>
2022-03-21 10:46:59 +0000

    Push from SwaggerHub.

--- node-output/controllers/Opportunity.js
+++ node-output/controllers/Opportunity.js
@@ -3,8 +3,8 @@
 var utils = require('../utils/writer.js');
 var Opportunity = require('../service/OpportunityService');
 
-module.exports.dashboardMetaDataGET = function dashboardMetaDataGET (req, res, next) {
-  Opportunity.dashboardMetaDataGET()
+module.exports.dashboardMetaData = function dashboardMetaData (req, res, next) {
+  Opportunity.dashboardMetaData()
     .then(function (response) {
       utils.writeJson(res, response);
     })
